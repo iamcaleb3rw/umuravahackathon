b310f7942554edfe2924fb977cea395ac82df89f
Caleb Ganza <icaleb130@gmail.com>
2025-01-22 18:21:34 +0200

    Removal of unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { DM_Sans } from "next/font/google";
+
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const dmSans = DM_Sans({
-  subsets: ["latin"], // Specify the subsets you need
-  weight: ["400", "500", "700"], // Include the weights you use
-  style: ["normal", "italic"], // Optional, add italic if needed
-  variable: "--font-dm-sans", // Optional: use a custom CSS variable
-});
 const workSans = Work_Sans({
   subsets: ["latin"], // Specify the subsets you need
   weight: ["400", "500", "700"], // Include the weights you use
   style: ["normal", "italic"], // Optional, add italic if needed
   variable: "--font-dm-sans", // Optional: use a custom CSS variable
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
