d5ce7f292b7d39816266645453e5caa2b01f8aa1
Caleb Ganza <icaleb130@gmail.com>
2025-01-25 14:58:09 +0200

    Removing unnecessary test borders

--- app/(routes)/hackathons/page.tsx
+++ app/(routes)/hackathons/page.tsx
@@ -8,7 +8,7 @@ const Hackathons = () => {
     <div className="border-t py-3 md:py-24 px-2 sm:px-6 md:px-16">
       <div className="flex gap-4">
         <div className="flex items-center gap-2 group">
-          <div className="w-fit p-1 rounded-md flex items-center transition-transform justify-center group-hover:-translate-x-1">
+          <div className="border w-fit p-1 rounded-md flex items-center transition-transform justify-center group-hover:-translate-x-1">
             <MoveLeft className="h-3 w-3 text-muted-foreground" />
           </div>
           <Link href={"/"}>
@@ -24,7 +24,7 @@ const Hackathons = () => {
           </div>
         </div>
       </div>
-      <div className="border my-4 md:my-14 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  2xl:grid-cols-4 gap-3">
+      <div className="my-4 md:my-14 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  2xl:grid-cols-4 gap-3">
         <ChallengeCard
           status={"Open"}
           title={"Design a dashboard for SokoFund"}
